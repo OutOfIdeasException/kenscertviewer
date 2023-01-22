c7351648ac56360474d7c1b0bb25b93e6e7757c9
Ken <109365944+OutOfIdeasException@users.noreply.github.com>
2023-01-22 11:29:54 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,158 @@
+var viewURL = " ";
+
+function w3WebDev() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/w3schools-webappdev.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://verify.w3schools.com/1MYUD6HUHM';
+  viewURL = 'https://verify.w3schools.com/1MYUD6HUHM';
+}
+
+function w3FrontEnd() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/w3schools-frontenddev.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://verify.w3schools.com/1MYUD6FGR4';
+  viewURL = 'https://verify.w3schools.com/1MYUD6FGR4';
+}
+
+function w3html() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/w3schools-html.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://verify.w3schools.com/1MYHQJPK8Q';
+  viewURL = 'https://verify.w3schools.com/1MYHQJPK8Q';
+}
+
+function w3css() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/w3schools-css.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://verify.w3schools.com/1MWTPI27W0';
+  viewURL = 'https://verify.w3schools.com/1MWTPI27W0';
+}
+
+function w3js() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/w3schools-javascript.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://verify.w3schools.com/1MYUD6819X';
+  viewURL = 'https://verify.w3schools.com/1MYUD6819X';
+}
+
+function w3php() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/w3schools-php.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://verify.w3schools.com/1MYUD6ALQP';
+  viewURL = 'https://verify.w3schools.com/1MYUD6ALQP';
+}
+
+function w3sql() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/w3schools-sql.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://verify.w3schools.com/1MYUD6CWI3';
+  viewURL = 'https://verify.w3schools.com/1MYUD6CWI3';
+}
+
+function soloHTML() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/sololearn-html.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://www.sololearn.com/certificates/CT-GTQ1XEGJ';
+  viewURL = 'https://www.sololearn.com/certificates/CT-GTQ1XEGJ';
+}
+
+function soloCSS() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/sololearn-css.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://www.sololearn.com/certificates/CT-X0NY8TWC';
+  viewURL = 'https://www.sololearn.com/certificates/CT-X0NY8TWC';
+}
+
+function soloJS() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/sololearn-javascript.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://www.sololearn.com/certificates/CT-XU1SWVLN';
+  viewURL = 'https://www.sololearn.com/certificates/CT-XU1SWVLN';
+}
+
+function soloWebDev() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/sololearn-webdevfundamentals.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://www.sololearn.com/certificates/CT-VGF8POYU';
+  viewURL = 'https://www.sololearn.com/certificates/CT-VGF8POYU';
+}
+
+function soloRespWebDev() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/sololearn-responsivewebdesign.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://www.sololearn.com/certificates/CT-ASXBDRWQ';
+  viewURL = 'https://www.sololearn.com/certificates/CT-ASXBDRWQ';
+}
+
+function soloGameDevJS() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/sololearn-jsgamedev.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://www.sololearn.com/certificates/CT-TX05AFSO';
+  viewURL = 'https://www.sololearn.com/certificates/CT-TX05AFSO';
+}
+
+function soloPHP() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/sololearn-php.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://www.sololearn.com/certificates/CT-HYHSAPUD';
+  viewURL = 'https://www.sololearn.com/certificates/CT-HYHSAPUD';
+}
+
+function soloJQuery() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/sololearn-jquery.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://www.sololearn.com/certificates/CT-YSUPGUDP';
+  viewURL = 'https://www.sololearn.com/certificates/CT-YSUPGUDP';
+}
+
+function soloSQL() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/sololearn-sql.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://www.sololearn.com/certificates/CT-EQDZMSZ9';
+  viewURL = 'https://www.sololearn.com/certificates/CT-EQDZMSZ9';
+}
+
+function soloCSharp() {
+  document.getElementById("noCertSelected").style.display = "none";
+  document.getElementById("certSelected").style.display = "block";
+  document.getElementById("certImg").src = "images/sololearn-csharp.png";
+  document.getElementById("certImg").width = "350";
+  document.getElementById("urlDisplay").innerHTML = 'https://www.sololearn.com/certificates/CT-TUTTCRV9';
+  viewURL = 'https://www.sololearn.com/certificates/CT-TUTTCRV9';
+}
+
+function viewCert() {
+  window.open(viewURL, "_blank");
+}
